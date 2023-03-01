0ce93dd61d7f652b0fbe62539f71e8139945d225
SSShee77y <46356943+SSShee77y@users.noreply.github.com>
2023-03-01 16:12:41 -0500

    Completed splash-screen-feature

--- Assets/Scripts/SplashScreen.cs
+++ Assets/Scripts/SplashScreen.cs
@@ -1,18 +1,69 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class SplashScreen : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Image Fading")]
+    public Image FadeImage;
+    public float TimeToStart = 1f;
+    public float FadeInTime = 0.5f;
+    public float SplashScreenTime = 1f;
+    public float FadeOutTime = 0.5f;
+    [Header("Scene Loading")]
+    public string SceneNameToLoadInto;
+
+    private float _timeToStart;
+    private float _fadeInTime;
+    private float _splashScreenTime;
+    private float _fadeOutTime;
+    private bool _attemptedSceneLoad;
+
     void Start()
     {
-        
+        Color fixedColor = FadeImage.color;
+        fixedColor.a = 1;
+        FadeImage.color = fixedColor;
+        FadeImage.CrossFadeAlpha(1f, 0f, true);
+
+        _timeToStart = TimeToStart;
+        _fadeInTime = FadeInTime;
+        _splashScreenTime = SplashScreenTime;
+        _fadeOutTime = FadeOutTime;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (_timeToStart > 0f)
+        {
+            _timeToStart -= Time.deltaTime;
+        }
+        else if (_fadeInTime > 0f)
+        {
+            FadeImage.CrossFadeAlpha(0f, FadeInTime, false);
+            _fadeInTime -= Time.deltaTime;
+        }
+        else if (_splashScreenTime > 0f)
+        {
+            _splashScreenTime -= Time.deltaTime;
+        }
+        else if (_fadeOutTime > 0f)
+        {
+            FadeImage.CrossFadeAlpha(1f, FadeOutTime, false);
+            _fadeOutTime -= Time.deltaTime;
+        }
+        else if (_attemptedSceneLoad == false)
+        {
+            if (SceneNameToLoadInto != null && SceneNameToLoadInto.Length > 0)
+            {
+                Invoke("LoadNextScene", FadeOutTime * 1.5f);
+            }
+            _attemptedSceneLoad = true;
+        }
+    }
+
+    void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneNameToLoadInto, LoadSceneMode.Single);
     }
 }
